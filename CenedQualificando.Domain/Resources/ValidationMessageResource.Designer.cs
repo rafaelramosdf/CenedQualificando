d9a0b99d2fd9cf65b5b41a2a7c3bf87a17f2cd88
Rafael Ramos <rafaelramosdf@gmail.com>
2022-05-26 12:53:07 -0300

    Correcao Resources

--- CenedQualificando.Domain/Resources/ValidationMessageResource.Designer.cs
+++ CenedQualificando.Domain/Resources/ValidationMessageResource.Designer.cs
@@ -19,7 +19,7 @@ namespace CenedQualificando.Domain.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ValidationMessageResource {
@@ -61,7 +61,7 @@ namespace CenedQualificando.Domain.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to O campo {0} é obrigatório.
+        ///   Looks up a localized string similar to Informe o campo &quot;{0}&quot;.
         /// </summary>
         public static string CampoObrigatorio {
             get {
